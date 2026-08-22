98b6327595a831c146c7b412fc1f235ee82740a0
wafflepug <rsinchongco28@gmail.com>
2026-08-22 16:35:30 +1000

    Bust navigation cleanup loader cache

--- waffle-v11.1.14.js
+++ waffle-v11.1.14.js
@@ -137,7 +137,7 @@
 
     if (!document.querySelector('script[data-waffle-v11117]')) {
       const script = document.createElement('script');
-      script.src = 'waffle-v11.1.17.js?v=11.1.17.4';
+      script.src = 'waffle-v11.1.17.js?v=11.1.17.5';
       script.async = false;
       script.setAttribute('data-waffle-v11117', 'js');
       document.body.appendChild(script);
