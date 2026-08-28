76ac0b5d65534ee10422910b5ecef04b0bbb4eff
wafflepug <rsinchongco28@gmail.com>
2026-08-28 23:53:37 +1000

    Load confirmed stay profile actions

--- waffle-bootstrap.js
+++ waffle-bootstrap.js
@@ -31,6 +31,7 @@
       "waffle-v11.1.95.js",
       "waffle-v11.1.96.js",
       "waffle-v11.1.97.js",
+      "waffle-v11.1.98.js",
       "waffle-v11.1.94.js"
 ];
   const maintenanceUrl = new URL('maintenance.html', window.location.href);
