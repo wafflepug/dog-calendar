f135fd1bf85959d47f1f06bfe4f496d236265f98
wafflepug <rsinchongco28@gmail.com>
2026-08-30 18:25:53 +1000

    Release A: add isolated R&D configuration seam

---
+++ rnd/config.js
@@ -0,0 +1,7 @@
+// Release A R&D only. Publishable Supabase values may be placed here after
+// connecting the dedicated free R&D project. Never place service-role keys here.
+window.WAFFLE_RND_CONFIG = Object.freeze({
+  environment: 'rnd',
+  supabaseUrl: '',
+  supabaseAnonKey: ''
+});
